b55a4436df0cf9098cb4f0fd2517cd9d66d03e8b
bison-- <mike.bison42@googlemail.com>
2022-01-22 11:17:39 +0100

    scale directory mod

---
+++ python/helper.py
@@ -0,0 +1,25 @@
+
+def bool_conversion(user_input):
+    valid_true = {'true', 'True', 'y', 'yes', 'j', 'ja'}
+    return user_input in valid_true
+
+
+def valid_input(question, _type=str, use_default=False, default=None):
+    need_input = True
+    result = None
+    while need_input:
+        user_input = input(question)
+        if user_input == '' and use_default:
+            result = default
+            break
+
+        if _type == bool:
+            user_input = bool_conversion(user_input)
+
+        try:
+            result = _type(user_input)
+            need_input = False
+        except Exception as ex:
+            print(ex)
+
+    return result
--- python/scaleDirectory.py
+++ python/scaleDirectory.py
@@ -1,9 +1,12 @@
 import os
 from PIL import Image
+import helper
+
 
 source_path = input('enter source path:').strip()
 destination_path = input('enter destination path:').strip()
 width = int(input('enter max width:').strip())
+rename_files = helper.valid_input('rename files? ', bool, True, False)
 
 only_files = [f for f in os.listdir(source_path) if os.path.isfile(os.path.join(source_path, f))]
 
@@ -11,7 +14,9 @@ only_files = [f for f in os.listdir(source_path) if os.path.isfile(os.path.join(
 count = 1
 for _file_name in only_files:
     full_source_path = os.path.join(source_path, _file_name)
-    full_destination_path = os.path.join(destination_path, "card_" + str(count) + ".png")
+    full_destination_path = os.path.join(destination_path, _file_name)
+    if rename_files:
+        full_destination_path = os.path.join(destination_path, "card_" + str(count) + ".png")
 
     try:
         img = Image.open(full_source_path)
@@ -20,5 +25,10 @@ for _file_name in only_files:
         img = img.resize((width, height_size), Image.ANTIALIAS)
         img.save(full_destination_path)
         count += 1
+        if width == height_size:
+            print('Square:', (width, height_size), _file_name)
+        else:
+            print('NOT Square:', (width, height_size), _file_name)
+
     except Exception as ex:
         print(ex)
