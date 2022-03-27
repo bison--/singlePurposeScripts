4aaa85432d785263cd003c4ebdb81f4e5c10fcb0
bison-- <mike.bison42@googlemail.com>
2022-03-27 07:52:51 +0200

    wordl wordlist generator

---
+++ python/worldWordlistDownloader.py
@@ -0,0 +1,35 @@
+import os
+import urllib.request
+
+# https://github.com/dwyl/english-words
+source_url = 'https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt'
+raw_word_file = 'temp/wordl_dl.txt'
+target_word_file = 'output/world_wordlist.txt'
+
+
+def do_download(url, dest):
+    url_handle = urllib.request.urlopen(url)
+    open(dest, 'w').write(url_handle.read().decode('utf-8'))
+
+
+def get_filtered_words(words, filter):
+    filtered = []
+    for word in words:
+        if len(word.strip()) == filter:
+            filtered.append(word)
+
+    return filtered
+
+
+if not os.path.exists(raw_word_file):
+    print('DOWNLOADING FILE')
+    do_download(source_url, raw_word_file)
+else:
+    print('FILE EXISTS, SKIPPING DOWNLOAD')
+
+all_words = open(raw_word_file).readlines()
+
+match_chars = int(input('enter number of chars to filter words:'))
+print(match_chars)
+
+open(target_word_file, 'w').writelines(get_filtered_words(all_words, match_chars))
