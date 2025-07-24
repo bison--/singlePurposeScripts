a053bb3a7cc128bb1c2f7d8c2bf3353f68c7b8b8
bison-- <mike.bison42@googlemail.com>
2025-07-24 17:54:59 +0200

    make it more resilient

--- python/mp3tags2csv.py
+++ python/mp3tags2csv.py
@@ -8,7 +8,7 @@ import helper
 
 
 source_path = helper.valid_input('Enter path with MP3s:')
-dest_csv = helper.valid_input('Enter path and filename for target CSV (default output/track_metadata.csv):', True, 'output/track_metadata.csv')
+dest_csv = helper.valid_input('Enter path and filename for target CSV (default output/track_metadata.csv):', use_default=True, default='output/track_metadata.csv')
 
 csv_handle = open(dest_csv, 'w', encoding='UTF8', newline='\n')
 csv_writer = csv.writer(csv_handle)
@@ -24,19 +24,46 @@ csv_writer.writerow([
     "ISRC (optional)"
 ])
 
-for mp3file in os.listdir(source_path):
+counter = 0
+
+all_files = os.listdir(source_path)
+all_files = sorted(all_files)
+
+for mp3file in all_files:
     if not mp3file.endswith('.mp3'):
         continue
+
+    counter += 1
 
     file_path = os.path.join(source_path, mp3file)
     audio_tags = EasyID3(file_path)
     audio_stream = MP3(file_path)
     print(audio_tags)
+
+    track_number = counter
+
+    if 'tracknumber' in audio_tags:
+        track_number = audio_tags['tracknumber'][0]
+
+    title = audio_tags['title'][0]
+
+    # fix some title issues
+    #title = title.replace('Nipple', 'Nibble')
+
+    if '_' in title:
+        parts = title.split('_')
+
+        # track numbers are in front
+        if parts[0].isnumeric():
+            title = ' '.join(parts[1:])
+        else:
+            title = ' '.join(parts)
+
     csv_writer.writerow([
         1,
-        audio_tags['tracknumber'][0],
-        audio_tags['title'][0],
-        'de',
+        track_number,
+        title,
+        'en',
         '',
         #str(datetime.timedelta(seconds=audio_stream.info.length)),
         strftime("%M:%S", gmtime(audio_stream.info.length)),
